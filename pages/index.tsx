21c147ea9518043c84b7488b4b9eb3f6ed6f247c
Ahmad Alghali <a.alghali@primeboard.org>
2022-11-02 00:38:03 +0000

    added alt to images for prod build

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,7 @@ export default function Home() {
         <h1 className='font-semibold text-gray-600 text-start text-5xl'>Lorem ipsum dolor sit amet.</h1>
 
         <div className='flex justify-center mt-20'>
-          <Image src='/descriptions.svg' height='200' width='200' className='' />
+          <Image alt="image" src='/descriptions.svg' height='200' width='200' className='' />
         </div>
 
         <p className='text-center mt-10'>
@@ -23,8 +23,8 @@ export default function Home() {
       <div className='flex flex-col space-y-20  mt-20'>
         <div className='flex p-2'>
           <div className='flex justify-center w-1/3'>
-            <Image src='/goals.svg' height='100' width='100' className='' />
-          </div>
+            <Image alt="image" src='/goals.svg' height='100' width='100' className='' />
+          </div> 
           <p className='w-2/3'>Complete your goals and earn rewards.</p>
         </div>
 
@@ -32,7 +32,7 @@ export default function Home() {
           <p className='w-2/3'>Stay on track</p>
 
           <div className='flex justify-center w-1/3'>
-            <Image src='/check-list.svg' height='100' width='100' className='' />
+            <Image alt="image" src='/check-list.svg' height='100' width='100' className='' />
           </div>
         </div> */}
 
@@ -40,13 +40,13 @@ export default function Home() {
           <p className='w-2/3'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ea, at.</p>
 
           <div className='flex justify-center w-1/3'>
-            <Image src='/todo.svg' height='100' width='100' className='' />
+            <Image alt="image" src='/todo.svg' height='100' width='100' className='' />
           </div>
         </div>
 
         <div className='flex p-2'>
           <div className='flex justify-center w-1/3'>
-            <Image src='/feedback.svg' height='100' width='100' className='' />
+            <Image alt="image" src='/feedback.svg' height='100' width='100' className='' />
           </div>
           <p className='w-2/3'>Celebrate your achievements with friends, you earned it!</p>
         </div>
@@ -55,7 +55,7 @@ export default function Home() {
           <p className='w-2/3'>Get visual progress updates to stay on track.</p>
 
           <div className='flex justify-center w-1/3'>
-            <Image src='/progress.svg' height='100' width='100' className='' />
+            <Image alt="image" src='/progress.svg' height='100' width='100' className='' />
           </div>
         </div>
 
@@ -63,7 +63,7 @@ export default function Home() {
       </div>
 
       <div className='flex justify-center mt-20'>
-        <Image src='/social-groups.svg' height='300' width='300' className='' />
+        <Image alt="image" src='/social-groups.svg' height='300' width='300' className='' />
       </div>
       <p className='text-center mt-10'>Engage in social groups and challenge your friends!</p>
 
