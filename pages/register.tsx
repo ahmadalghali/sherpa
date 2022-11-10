333d5f1c60974a212dd6284c03bd086aa8b0c29d
Ahmad Alghali <ahmad-mustafaalghali@hotmail.com>
2022-11-10 01:51:36 +0000

    Working version of register page

--- pages/register.tsx
+++ pages/register.tsx
@@ -1,22 +1,81 @@
 import { useRouter } from "next/router";
-import React from "react";
+import React, { useState } from "react";
+import { useMutation } from "react-query";
 import NotLoggedInLayout from "../components/NotLoggedInLayout";
+import useAuth from "../hooks/useAuth";
+
+import { useForm, SubmitHandler } from "react-hook-form";
+
+type Inputs = {
+  email: string
+  username: string
+  password: string
+  confirmPassword: string
+};
 
 type Props = {};
 
 export default function Register({ }: Props) {
   const router = useRouter();
 
-  const register = () => {
-    console.log("Signing up...");
-  };
+  const { register: signUp } = useAuth()
+  const { mutate, isLoading } = useMutation(signUp);
+
+  const { register, handleSubmit, watch, formState: { errors }, setError } = useForm<Inputs>();
+
+  const onSubmit: SubmitHandler<Inputs> = ({ email, username, confirmPassword, password }) => {
+    if (confirmPassword !== password) return setError("confirmPassword", { message: "Passwords do not match" })
+    mutate({ email, username, password })
+
+  }
+
   return (
-    <>
-      <form className='h-screen flex flex-col justify-center max-w-lg mx-auto'>
-        <input type='email' className='input' placeholder='Email' />
-        <input type='password' className='input mt-5' placeholder='Password' />
-        <input type='password' className='input mt-5' placeholder='Confirm Password' />
-        <button type='submit' onClick={() => register} className='w-full btn btn-primary mt-10'>
+    <div className="h-screen flex flex-col justify-center space-y-10">
+      <h1 className="text-4xl text-center font-bold">Join Us</h1>
+      <form className='max-w-lg mx-auto' onSubmit={handleSubmit(onSubmit)}>
+        <div className="flex flex-col space-y-5">
+
+          <div>
+            <label htmlFor="email" className="ml-2">Email</label>
+            <input type="email" className='input mt-1'{...register("email", {
+              required: "Required",
+              pattern: {
+                value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                message: "Invalid email address"
+              }
+            })} />
+            {errors.email && <p className="text-red-600 ml-2 mt-1">{errors.email.message}</p>}
+          </div>
+
+          <div>
+            <label htmlFor="email" className="ml-2">Username</label>
+            <input type="text" className='input mt-1'{...register("username", {
+              required: "Required",
+            })} />
+            {errors.username && <p className="text-red-600 ml-2 mt-1">{errors.username.message}</p>}
+          </div>
+
+
+          <div>
+            <label htmlFor="email" className="ml-2">Password</label>
+            <input type="password" className='input mt-1'              {...register("password", { required: "Required", minLength: { value: 6, message: `Minimum 6 characters` } })} />
+            {errors.password && <p className="text-red-600 ml-2 mt-1">{errors.password.message}</p>}
+          </div>
+
+
+          <div>
+            <label htmlFor="email" className="ml-2">Confirm Password</label>
+            <input type="password" className='input mt-1'
+              {...register("confirmPassword", { required: "Required", minLength: { value: 6, message: `Minimum 6 characters` } })} />
+            {errors.confirmPassword && <p className="text-red-600 ml-2 mt-1">{errors.confirmPassword.message}</p>}
+          </div>
+        </div>
+
+        <ul className="hint text-sm text-gray-400 list-disc ml-5 mt-10">
+          <li>Password must be 6 characters or more</li>
+        </ul>
+
+        <button type='submit' className='w-full btn btn-primary mt-10'>
           Sign up
         </button>
 
@@ -27,13 +86,14 @@ export default function Register({ }: Props) {
           </span>
         </p>
       </form>
-    </>
+    </div>
   );
 }
 
 
 Register.getLayout = function getLayout(page: any) {
   return (
+
     <NotLoggedInLayout>
       {page}
     </NotLoggedInLayout>
