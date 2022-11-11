32768cc778ed649788ca08e555bedcf9e27035eb
Ahmad Alghali <ahmad-mustafaalghali@hotmail.com>
2022-11-11 02:10:55 +0000

    Added tailwind typography plugin to automatically style markdown articles

--- pages/privacy-policy.tsx
+++ pages/privacy-policy.tsx
@@ -5,65 +5,52 @@ type Props = {}
 
 function PrivacyPolicy({ }: Props) {
   return (
-    <div className='py-28'>
+    <article className='py-28 prose prose-li:marker:text-black'>
 
-      <h1 className="text-4xl text-center font-semibold">
+      <h1>
         Sherpa&apos;s Privacy Policy
       </h1>
 
 
-      <div className="space-y-16">
+      <p>Sherpa is a mobile application available for Android and iOS devices. Through this app, users are able to create personal goals and objectives.</p>
 
-        <p className=' mt-10'>Sherpa is a mobile application available for Android and iOS devices. Through this app, users are able to create personal goals and objectives.</p>
+      <p> Minimal amount of personal details is required to benefit from the services this App makes available.This Privacy Policy outlines what data we may collect, and how it&apos;s processed. </p>
 
-        <p> Minimal amount of personal details is required to benefit from the services this App makes available. This Privacy Policy outlines what data we may collect, and how it's processed. For any further information in respect to how we value your privacy feel free to contact us by email at sherpaadmin@gmail.com.</p>
-
-
-
-        <div className="space-y-5">
-          <p className='text-center font-semibold text-xl'>Data use</p>
-
-          <ul className='list-disc space-y-2'>
-            <li>Allowing you to generate content</li>
-            <li>Allowing you to engage with other users</li>
-            <li>Allowing you to store data for your future reuse.</li>
-          </ul>
-        </div>
+      <p>For any further information in respect to how we value your privacy feel free to contact us by email at <b>sherpaadmin@gmail.com</b>.</p>
 
 
-        <div className="space-y-5">
-          <p className='text-center font-semibold text-xl'>We do not sell your data</p>
+      <h2>Data use</h2>
 
-          <p>
-            We will never sell or attempt to sell the data you provide us. We guarantee that the data you trust us with will never be sold or shared as a commodity. We shall only use your data as a means to benefit you in respect to your use of this App.
-          </p>
-        </div>
+      <ul>
+        <li>Allowing you to generate content</li>
+        <li>Allowing you to engage with other users</li>
+        <li>Allowing you to store data for your future reuse.</li>
+      </ul>
 
+      <h2>We do not sell your data</h2>
 
-        <div className="space-y-5">
-
-          <p className='text-center font-semibold text-xl'>Your rights</p>
-
-          <p>As a user of our service, you maintain the right to: </p>
-
-          <ul className='list-disc space-y-2'>
-            <li>Amend or delete data you have provided us</li>
-            <li>Request us to amend or delete your data</li>
-            <li>Request us to provide any data we have associated with your account</li>
-          </ul>
-        </div>
+      <p>
+        We will never sell or attempt to sell the data you provide us. We guarantee that the data you trust us with will never be sold or shared as a commodity. We shall only use your data as a means to benefit you in respect to your use of this App.
+      </p>
 
 
-        <div className="space-y-5">
-          <p className='text-center font-semibold text-xl'>Contact us</p>
+      <h2>Your rights</h2>
 
-          <p>
-            for further details, please reach the team via email: sherpaadmin@gmail.com
-          </p>
-        </div>
+      <p>As a user of our service, you maintain the right to: </p>
 
-      </div>
-    </div >
+      <ul >
+        <li>Amend or delete data you have provided us</li>
+        <li>Request us to amend or delete your data</li>
+        <li>Request us to provide any data we have associated with your account</li>
+      </ul>
+
+      <h2>Contact us</h2>
+
+      <p>
+        For further details, please reach the team via email: <b>sherpaadmin@gmail.com</b>
+      </p>
+
+    </article >
   )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -18,6 +18,7 @@ module.exports = {
       }
     }
   },
+  plugins: [require('@tailwindcss/typography'),]
 //   plugins: [require("daisyui")],
 //   daisyui: {
 //     themes: [ 
