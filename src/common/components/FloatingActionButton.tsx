95c8d00ebcc3d459fd35bdce7c5b7d4503627259
Ahmad Alghali <ahmad-mustafaalghali@hotmail.com>
2022-11-17 21:08:29 +0000

    fix responsiveness of FAB

--- src/common/components/FloatingActionButton.tsx
+++ src/common/components/FloatingActionButton.tsx
@@ -8,11 +8,10 @@ type Props = {
 function FloatingActionButton({ text, className }: Props) {
   return (
     <button
-      className={`btn-primary drop-shadow-2xl hover:scale-110 hover:-translate-y-1 duration-200 transition-all  inset-x-0 fixed bottom-24 z-20 max-w-md mx-5 lg:mx-auto ${className}`}
+      className={`btn-primary drop-shadow-2xl hover:scale-110 hover:-translate-y-1 duration-200 transition-all  inset-x-0 fixed bottom-24 z-20 max-w-xs sm:max-w-sm  mx-auto ${className}`}
     >
       {text}
     </button>
-
     // <motion.button
     //   initial={{ y: '100vh' }}
     //   animate={{ y: 0 }}
