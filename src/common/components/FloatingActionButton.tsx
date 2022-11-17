02fab3bf7644bd4a6cf15131f57da257dfb9e2b1
Ahmad Alghali <ahmad-mustafaalghali@hotmail.com>
2022-11-17 20:08:02 +0000

    Add reusable FAB component

---
+++ src/common/components/FloatingActionButton.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+type Props = {
+  text: string;
+  className?: string;
+};
+
+function FloatingActionButton({ text, className }: Props) {
+  return (
+    <button
+      className={`btn-primary drop-shadow-2xl hover:scale-110 hover:-translate-y-1 duration-200 transition-all  inset-x-0 fixed bottom-24 z-20 max-w-md mx-5 lg:mx-auto ${className}`}
+    >
+      {text}
+    </button>
+
+    // <motion.button
+    //   initial={{ y: '100vh' }}
+    //   animate={{ y: 0 }}
+    //   className={`btn-accent drop-shadow-2xl hover:scale-110 hover:-translate-y-1 duration-200 transition-all ${className}`}>
+    //   Add a new goal
+    // </motion.button>
+  );
+}
+
+export default FloatingActionButton;
