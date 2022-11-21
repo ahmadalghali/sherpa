9136ea16639c36f865cbb3174bc97236d7ddfaaf
Ahmad Alghali <ahmad-mustafaalghali@hotmail.com>
2022-11-21 13:54:54 +0000

    reduce padding x on bottom navbar

--- src/common/components/BottomNavigationBar.tsx
+++ src/common/components/BottomNavigationBar.tsx
@@ -21,7 +21,7 @@ export default function BottomNavigationBar() {
     }
   };
   return (
-    <div className="h-16 bg-accent-900 text-white px-6 fixed bottom-0 left-0 right-0">
+    <div className="h-16 bg-accent-900 text-white px-2 fixed bottom-0 left-0 right-0">
       <div className="flex justify-around h-full text-3xl items-center relative">
         <GlobeAmericasIcon className="h-7" />
         <MagnifyingGlassIcon className="h-7" />
