b33eee698bd43813970b2f76d57192c30511004b
Ahmad Alghali <ahmad-mustafaalghali@hotmail.com>
2022-11-23 19:55:49 +0000

    add cursur-pointer to bottom navbar icons

--- src/common/components/BottomNavigationBar.tsx
+++ src/common/components/BottomNavigationBar.tsx
@@ -23,15 +23,15 @@ export default function BottomNavigationBar() {
   return (
     <div className="h-16 bg-accent-900 text-white px-2 fixed bottom-0 left-0 right-0">
       <div className="flex justify-around h-full text-3xl items-center relative">
-        <GlobeAmericasIcon className="h-7" />
-        <MagnifyingGlassIcon className="h-7" />
+        <GlobeAmericasIcon className="h-7 cursor-pointer" />
+        <MagnifyingGlassIcon className="h-7 cursor-pointer" />
         <PlusIcon
-          className="h-12 p-1 bg-brand text-white rounded-full shadow-2xl absolute bottom-3"
+          className="h-12 p-1 bg-brand cursor-pointer text-white rounded-full shadow-2xl absolute bottom-3"
           onClick={() => notify()}
         />
-        <PlusIcon className="invisible h-12 p-1 bg-brand text-white rounded-full shadow-2xl" />
-        <HomeIcon className="h-7" />
-        <UserIcon className="h-7" />
+        <PlusIcon className="invisible disabled h-12 cursor-pointer p-1 bg-brand text-white rounded-full shadow-2xl" />
+        <HomeIcon className="h-7 cursor-pointer" />
+        <UserIcon className="h-7 cursor-pointer" />
       </div>
     </div>
   );
